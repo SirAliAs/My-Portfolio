6079dbb1cf19d46418e578fc7d0113e8c25c4fc7
Ali Astan <ali.astanboos@gmail.com>
2023-10-29 02:05:15 +0330

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,184 @@
+/*Intro animation*/
+const intro = document.querySelector(".intro");
+const logo = document.querySelector(".logo-header");
+const logoSpan = document.querySelectorAll(".logo");
+
+window.addEventListener("DOMContentLoaded", () => {
+  logoSpan.forEach((span, idx) => {
+    setTimeout(() => {
+      span.classList.add("appear");
+    }, (idx + 1) * 90);
+  });
+
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.remove("appear");
+        span.classList.add("fade");
+      }, (idx + 1) * 80);
+    });
+  }, 1500);
+
+  setTimeout(() => {
+    intro.style.top = "-100vh";
+  }, 2000);
+});
+
+/*Show menu*/
+const showMenu = (toggleId, navID) => {
+  const toggle = document.getElementById(toggleId);
+  nav = document.getElementById(navID);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show_menu");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/*Active and remove menu*/
+const navLink = document.querySelectorAll(".nav_link");
+
+function linkAction() {
+  navLink.forEach(n => n.classList.remove("active"));
+  this.classList.add("active");
+
+  /*This is for removing menu on click on mobile*/
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show_menu");
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction));
+
+/*Auto write text*/
+
+var app = document.getElementById("autoWrite");
+
+var typewriter = new Typewriter(app, {
+  strings: [
+    "React.js Developer",
+    "Former English Teacher",
+    "Telecom Engineer",
+    "Nature Lover",
+    "HandPan Player",
+  ],
+  loop: true,
+  autoStart: true,
+});
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "40px",
+  duration: 2000,
+  reset: true,
+});
+
+/*SCROLL HOME*/
+sr.reveal(".home_title", {});
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home_img", { delay: 400 });
+sr.reveal(".home_social-icon", { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal(".about_img", {});
+sr.reveal(".about_subtitle", { delay: 400 });
+sr.reveal(".about_text", { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal(".skills_subtitle", {});
+sr.reveal(".skills_text", {});
+sr.reveal(".skills_data", { interval: 200 });
+sr.reveal(".skills_img", { delay: 600 });
+
+/*SCROLL WORK*/
+sr.reveal(".work_img", { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal(".contact_input", { interval: 200 });
+
+/*Toggle dark mode*/
+const toggleDarkMode = document.getElementById("toggle");
+
+toggleDarkMode.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+  document.getElementById("head").classList.toggle("dark");
+});
+document.body.classList.toggle("dark");
+document.getElementById("head").classList.toggle("dark");
+
+/*Particles.js*/
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector(".js-count-particles");
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
